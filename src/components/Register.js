8edbe9de1e2cc339b1ce3ab0f914f5208c88eb77
Jogendra Pattnaik <jogendrapattnaik4@gmail.com>
2023-01-24 09:36:08 +0530

    Register page added

---
+++ src/components/Register.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Register.css'
+import { useState } from 'react';
+const Register = ()=>{
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [message,setMessage] = useState("");
+  
+    let handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        let res = await fetch("http://localhost:4001/createnote", {
+          headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          method: "POST",
+          body: JSON.stringify({
+            title: title,
+            content: content,
+            isCompleted: false,
+          }),
+        });
+        let resJson = await res.json();
+        if (res.status === 200) {
+          setTitle("");
+          setContent("");
+          setMessage("User created successfully");
+        } else {
+          setMessage("Some error occured");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    return (
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            placeholder="Note Title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            value={content}
+            placeholder="Note Description"
+            onChange={(e) => setContent(e.target.value)}
+          />  
+          <button type="submit">Create</button>
+  
+          <div className="message">{message ? <p>{message}</p> : null}</div>
+        </form>
+      </div>
+    );
+}
+
+export default Register;
